1b00b240f91964ef5351674140341439749c9529
dimaldo86 <dimaldo86ne@mail.ru>
2022-09-24 05:32:19 +0700

    Сделал меню, выпадающее меню

--- src/js/app.js
+++ src/js/app.js
@@ -1,14 +1,32 @@
 window.addEventListener('DOMContentLoaded', () => {
     /** modal */
-    const modal = (modalTrigger, modalContent) => {
+    const modal = (modalTrigger, modalContent, visibleClass, hideClass) => {
         const trigger = document.querySelector(modalTrigger),
-              content = document.querySelector(modalContent);
+              content = document.querySelector(modalContent),
+               htmlElement = document.querySelector("html");
+
+
 
         trigger.addEventListener('click', () => {
-            content.classList.toggle('visible')
+            content.classList.toggle(visibleClass)
+            content.classList.add(hideClass)
+        })
+
+
+
+        window.addEventListener('click', (e) => {
+            if (e.target === document.htmlElement) {
+                content.classList.remove('visible')
+            }
+
+
+
+
         })
     }
 
-    modal('.top__location', '.modal__wrapper')
+    modal('.top__location', '.modal__wrapper', 'visible', 'hide')
+
+
 
 })
